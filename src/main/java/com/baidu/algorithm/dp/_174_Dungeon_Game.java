3bf0aa520935849a98823f49ba7f14f39f455912
hrxu <562963269@qq.com>
2017-03-22 11:56:00 +0800

    add

---
+++ src/main/java/com/baidu/algorithm/dp/_174_Dungeon_Game.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package com.baidu.algorithm.dp;
+
+/**
+ * _174_Dungeon_Game
+ *
+ * @author xuhaoran01
+ */
+public class _174_Dungeon_Game {
+
+    public int calculateMinimumHP(int[][] dungeon) {
+
+        int m = dungeon.length, n = dungeon[0].length;
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = Math.max(1, 1 - dungeon[m - 1][n - 1]);
+
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n - 1] = Math.max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            dp[m - 1][i] = Math.max(1, dp[m - 1][i + 1] - dungeon[m - 1][i]);
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = Math.min(Math.max(dp[i + 1][j] - dungeon[i][j], 1),
+                        Math.max(dp[i][j + 1] - dungeon[i][j], 1));
+            }
+        }
+
+        return dp[0][0];
+    }
+}
